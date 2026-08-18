cf71b895be34a46978421e1e5224cfec4fb604fd
Caitlynn-Aug <caitxl@gmail.com>
2026-08-18 16:01:43 +0800

    Add files via upload

---
+++ 01-about-me.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "=========================================" << endl;
+    cout << "My name is Caitlynn Aug Xue Le" << endl;
+    cout << "My matric id is 10DIT26F1156" << endl;
+    cout << "My course is Diploma in Information Technology" << endl;
+    cout << "Welcome to my GitHub! Today is August 18, 2026." << endl;
+    cout << "=========================================" << endl;
+
+    return 0;
+}
