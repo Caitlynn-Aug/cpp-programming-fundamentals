ffe21b269f1bcf6590be0f15423026a6e3f969cd
Caitlynn-Aug <caitxl@gmail.com>
2026-08-18 16:18:25 +0800

    Add files via upload

---
+++ 02-assignment1-cover.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "*********************************************************" << endl;
+    cout << "\t\tPoliteknik Seberang Perai\n";
+    cout << "Department of Information and Communication Technology\n";
+    cout << "**********************************************************" << endl;
+  
+    return 0;
+}
