bdaa8a12259714d83ca795a7c2354e07060bb5c2
Caitlynn-Aug <caitxl@gmail.com>
2026-08-20 00:14:45 +0800

    Add files via upload

---
+++ 05-assignment4-cover.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#define pi 3.142
+
+int main()
+{
+    float r,area;
+    cout<<"Enter radius of the circle in cm:";
+    cin >> r;
+    area =pi*r*r;
+    cout<<"area of the circle  is "<<area<<"cm aquare"<<endl;
+
+    return 0;
+}
