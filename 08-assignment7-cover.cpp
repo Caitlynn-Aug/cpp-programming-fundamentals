abd13d23149f0e16d0c6122c840fb120deba0f79
Caitlynn-Aug <caitxl@gmail.com>
2026-08-21 12:18:02 +0800

    Add files via upload

---
+++ 08-assignment7-cover.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a[2][4] = {3,6,9,12,15,18,21,24};
+    cout <<"a[1][1]:"<<a[1][1]<<endl;
+    cout <<"a[1][3]:"<<a[1][3]<<endl;
+    
+    return 0;
+}
