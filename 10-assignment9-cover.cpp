aa33e230dab868ede1d98afffd8cc7afd63ab0ad
Caitlynn-Aug <caitxl@gmail.com>
2026-08-22 21:49:45 +0800

    Add files via upload

---
+++ 10-assignment9-cover.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float length,total;
+    
+    cout<<"length:";
+    cin >> length;
+    
+    total = 6*(length*length);
+    cout<<"Total surfac area:"<<total<<endl;
+
+    return 0;
+}
